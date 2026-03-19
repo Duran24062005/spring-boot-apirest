b75aa3012303095e839f4828e6f4e2edcb2f51cf
Alexi Dg <alexisdurangomez588@gmail.com>
2026-03-18 21:20:38 -0500

    Document product and sales endpoints

--- src/main/java/com/spring_boot/first_spring_boot_app/controllers/ProductController.java
+++ src/main/java/com/spring_boot/first_spring_boot_app/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.spring_boot.first_spring_boot_app.controllers;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring_boot.first_spring_boot_app.dtos.request.ProductRequestDto;
@@ -19,6 +19,8 @@ import com.spring_boot.first_spring_boot_app.dtos.response.ProductResponseDto;
 import com.spring_boot.first_spring_boot_app.service.ProductService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -33,34 +35,113 @@ public class ProductController {
     private final ProductService productService;
 
     @Operation(summary = "Listar productos")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Products successfully obtained"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid data or malformed request"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Authentication required"
+                    )
+            }
+    )
     @GetMapping
-    public List<ProductResponseDto> getProducts() {
-        return productService.findAll();
+    public ResponseEntity<List<ProductResponseDto>> getProducts() {
+        return ResponseEntity.ok().body(productService.findAll());
     }
 
     @Operation(summary = "Obtener producto por id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Product successfully obtained"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid data or malformed request"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Authentication required"
+                    )
+            }
+    )
     @GetMapping("/{id}")
-    public ProductResponseDto getProductById(@PathVariable Long id) {
-        return productService.findById(id);
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(productService.findById(id));
     }
 
     @Operation(summary = "Crear producto")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Product successfully created"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid data or malformed request"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Authentication required"
+                    )
+            }
+    )
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ProductResponseDto createProduct(@Valid @RequestBody ProductRequestDto request) {
-        return productService.create(request);
+    public ResponseEntity<ProductResponseDto> createProduct(@Valid @RequestBody ProductRequestDto request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(request));
     }
 
     @Operation(summary = "Actualizar producto")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Product successfully updated"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid data or malformed request"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Authentication required"
+                    )
+            }
+    )
     @PutMapping("/{id}")
-    public ProductResponseDto updateProduct(@PathVariable Long id, @Valid @RequestBody ProductRequestDto request) {
-        return productService.update(id, request);
+    public ResponseEntity<ProductResponseDto> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductRequestDto request) {
+        return ResponseEntity.ok().body(productService.update(id, request));
     }
 
     @Operation(summary = "Eliminar producto")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Product successfully deleted"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid data or malformed request"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Authentication required"
+                    )
+            }
+    )
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteProduct(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/spring_boot/first_spring_boot_app/controllers/SalesController.java
+++ src/main/java/com/spring_boot/first_spring_boot_app/controllers/SalesController.java
@@ -2,7 +2,7 @@ package com.spring_boot.first_spring_boot_app.controllers;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring_boot.first_spring_boot_app.dtos.request.SaleRequestDto;
 import com.spring_boot.first_spring_boot_app.dtos.response.SaleResponseDto;
 import com.spring_boot.first_spring_boot_app.service.SaleService;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -33,34 +34,113 @@ public class SalesController {
     private final SaleService saleService;
 
     @Operation(summary = "Listar ventas")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Sales successfully obtained"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid data or malformed request"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Authentication required"
+                    )
+            }
+    )
     @GetMapping
-    public List<SaleResponseDto> getSales() {
-        return saleService.findAll();
+    public ResponseEntity<List<SaleResponseDto>> getSales() {
+        return ResponseEntity.ok().body(saleService.findAll());
     }
 
     @Operation(summary = "Obtener venta por id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Sale successfully obtained"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid data or malformed request"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Authentication required"
+                    )
+            }
+    )
     @GetMapping("/{id}")
-    public SaleResponseDto getSaleById(@PathVariable Long id) {
-        return saleService.findById(id);
+    public ResponseEntity<SaleResponseDto> getSaleById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(saleService.findById(id));
     }
 
     @Operation(summary = "Crear venta")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Sale successfully created"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid data or malformed request"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Authentication required"
+                    )
+            }
+    )
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public SaleResponseDto createSale(@Valid @RequestBody SaleRequestDto request) {
-        return saleService.create(request);
+    public ResponseEntity<SaleResponseDto> createSale(@Valid @RequestBody SaleRequestDto request) {
+        return ResponseEntity.status(201).body(saleService.create(request));
     }
 
     @Operation(summary = "Actualizar venta")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Sale successfully updated"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid data or malformed request"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Authentication required"
+                    )
+            }
+    )
     @PutMapping("/{id}")
-    public SaleResponseDto updateSale(@PathVariable Long id, @Valid @RequestBody SaleRequestDto request) {
-        return saleService.update(id, request);
+    public ResponseEntity<SaleResponseDto> updateSale(@PathVariable Long id, @Valid @RequestBody SaleRequestDto request) {
+        return ResponseEntity.ok().body(saleService.update(id, request));
     }
 
     @Operation(summary = "Eliminar venta")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Sale successfully deleted"
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Invalid data or malformed request"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Authentication required"
+                    )
+            }
+    )
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteSale(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteSale(@PathVariable Long id) {
         saleService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
